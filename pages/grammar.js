1c20676854e7a96aaff211073dfc9800b7e120e5
josymarss <sylvajosymar@gmail.com>
2021-07-14 04:18:15 +0100

    Updated errors

--- pages/grammar.js
+++ pages/grammar.js
@@ -14,6 +14,11 @@ export default function Grammar(){
     return(
         <div className={grammar.container}>
            <h2>Alfabeto</h2>
+           <p className={grammar.intro}>{`O Kimbudu n'ao possi algumas letras, tais como: 
+               "C", "R", "G", "P" e "Q". A letra "g" e "p" são compostas
+               como mostra o lfatbeto em baixo, elas torna-se "ng" e "ph"
+               respetivamente. para palavras formadas com a letra "c" e "q"
+                usa-se a letra "k" em seu lugar .`}</p>
            <div className={grammar.alphabet}>
                 {alphabet.map((letter,index) => (
                     <p key={index}>{letter}</p>
--- pages/themecontext.js
+++
@@ -1,6 +0,0 @@
-// import { createContext } from 'react'
-// import theme from './theme.module.css'
-
-// const Theme = createContext(theme.default)
-
-// export default Theme

c2886b4b55914b912ac91ca0ee5d003911fa9f02
josymarss <sylvajosymar@gmail.com>
2022-02-03 19:23:40 +0100

    Version 1.1.0 is done

--- pages/numbers/cardinals.js
+++ pages/numbers/cardinals.js
@@ -1,12 +1,12 @@
 import NumberComponent from './numbercomponent';
 import styles from './numbers.module.css';
-import { dataNumbers } from './cardinalnumbers.js';
+import { dataNumbers } from '../../cardinalnumbers';
 
-export default function Cardinals(){
+export default function Cardinals( ){
       return(
             <div className={styles.container}>
                   <h2>Números Cardinais em kimbundu</h2>
-                  {dataNumbers.map(number,index => 
+                  {dataNumbers.map((number,index) => 
                         <NumberComponent key={index} number={number.key} value={number.value} />
                   )}
                   
--- pages/numbers/ordinals.js
+++ pages/numbers/ordinals.js
@@ -1,13 +1,13 @@
 import styles from './numbers.module.css';
 
-import { dataNumbersOrdinals } from './ordinalnumbers';
+import { dataNumbersOrdinals } from '../../ordinalnumbers';
 import NumberComponent from "./numbercomponent";
 
 export default function Ordinals(){
       return(
             <div className={styles.container}>
                   <h2>Números ordinais</h2>
-                  {dataNumbersOrdinals.map(number => 
+                  {dataNumbersOrdinals.map((number,index) => 
                        <NumberComponent key={index} number={number.key} value={number.value} /> 
                   )}
             </div>

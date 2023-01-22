a0e2c4f53df32b47e0d65b2eae6382fac55523bb
josymarss <sylvajosymar@gmail.com>
2023-01-22 22:32:13 +0100

    Done

--- components/banner.js
+++ components/banner.js
@@ -7,8 +7,8 @@ export default function Banner(){
     // bg-[url('/bg-image.png')]
     return(
         // bg-[url('/bg-image.png')]
-        <div className="bg-slate-500  w-full h-screen bg-no-repeat ">
-            <Image alt='banner' src='/bg-image.png' className='w-full object-contain'/>
+        <div className="bg-slate-500  w-full h-screen  ">
+            <div className=" bg-[url('/bg-image.png')] bg-cover w-full h-screen bg-no-repeat"></div>
             {/* <button>Come;ar agora</button> */}
         </div>
     );

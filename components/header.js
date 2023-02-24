16112f1369c39b9f6662dcbff14510f65e884401
rniochy <lniochy@gmail.com>
2023-02-24 05:35:27 +0100

    styling header to responsive layout

--- components/header.js
+++ components/header.js
@@ -23,61 +23,63 @@ export function Header (){
 
     return(
         <>
-        <header className='sticky top-0 z-40 bg-slate-100 px-5 flex items-center justify-between flex-row w-full h-[60px] shadow-lg font-mulish'>
-            <h2 className="font-bold text-2xl text-slate-800">
+        <header className='sticky top-0 z-40 bg-slate-100 px-5 flex items-center w-full h-[60px] shadow-lg font-mulish'>
+            <h2 className="font-bold text-2xl text-slate-800 w-[10%]">
                 <Link href='/'>Zwelar</Link>
             </h2>
-            <nav>
-            <ul className=' flex flex-row gap-[1.2rem]'>
-                <li className=" text-md hover:text-green-400">
-                    <Link href='/'>{`Home`}</Link>
-                </li>
-                <li className="text-md hover:text-green-400">
-                    <Link href='/grammar/grammar'>{`Gramática`}</Link>
-                </li>
-                <li className="text-md hover:text-green-400">
-                    <Link href='/music'>{`Música`}</Link>
-                </li>
-                <li className="text-md hover:text-green-400">
-                    <Link href='/history'>{`Histórias`}</Link>
-                </li>
-                <li className="text-md hover:text-green-400">
-                    <Link href='/courses'>{`Cursos`}</Link>
-                </li>
-                <li className=" text-md hover:text-green-400">
-                    <Link href='/about/about'>Sobre</Link>
-                </li>
-                <li  className=" text-md hover:text-green-400">
-                    <Link href='/team'>Criadores</Link>
-                </li>
-                <li  className=" text-md hover:text-green-400">
-                    <Link href='#'>Perfil</Link>
-                </li>
-                <li  className="flex text-md hover:text-green-400">
-                    <span className='mr-2'>
-                        <FontAwesomeIcon icon={faCodeBranch}></FontAwesomeIcon>
-                    </span>
-                    <span className=''>
-                        <Link href='https://github.com/josymarss/kimbundu-technical-expressions'>
-                            <a target="_blank">Github</a>
-                        </Link>
-                    </span> 
-                </li>
-                <li className="text-md hover:text-green-400">
-                    <span className='mr-2'>
-                        <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
-                    </span>
-                </li>
-            </ul>
-            </nav>
-            <div className='flex'>
-                <input 
-                    className='p-1 rounded-full text-center'
-                    type='text' 
-                    name='search' 
-                    placeholder='Sugerir palavra'
-                />
-                <button className='w-[5rem] hover:text-white p-1 rounded-full text-black ml-2 bg-green-400'onClick={onSugerir}>Enviar</button>
+            <div className='flex justify-between w-[90%]'>
+                <nav className='w-[80%]'>
+                    <ul className='flex flex-row gap-[1rem]'>
+                        <li className=" text-md header-li">
+                            <Link href='/'>{`Home`}</Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/grammar/grammar'>{`Gramática`}</Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/music'>{`Música`}</Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/history'>{`Histórias`}</Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/courses'>{`Cursos`}</Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/about/about'>Sobre</Link>
+                        </li>
+                        <li  className="header-li">
+                            <Link href='/team'>Criadores</Link>
+                        </li>
+                        <li  className="header-li">
+                            <Link href='#'>Perfil</Link>
+                        </li>
+                        <li  className="flex header-li">
+                            <span className='mr-2'>
+                                <FontAwesomeIcon icon={faCodeBranch}></FontAwesomeIcon>
+                            </span>
+                            <span className=''>
+                                <Link href='https://github.com/josymarss/kimbundu-technical-expressions'>
+                                    <a target="_blank">Github</a>
+                                </Link>
+                            </span> 
+                        </li>
+                        <li className="header-li">
+                            <span>
+                                <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='flex w-[20%]'>
+                    <input 
+                        className='p-1 outline-none rounded-full text-center text-[13px] w-[60%]'
+                        type='text' 
+                        name='search' 
+                        placeholder='Sugerir palavra'
+                    />
+                    <button className='w-[30%] hover:text-white px-2 rounded-full text-black ml-2 bg-green-400 text-[12px] 'onClick={onSugerir}>Enviar</button>
+                </div>
             </div>
         </header>
         {router.pathname === '/' ?

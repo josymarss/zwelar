076e50712c107afe14754b77b43a1df086beea0a
rniochy <lniochy@gmail.com>
2023-03-01 00:31:35 +0100

    building meu

--- components/header.js
+++ components/header.js
@@ -1,7 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faMoon,faCodeBranch, faHome } from "@fortawesome/free-solid-svg-icons";// Import the FontAwesomeIcon component
+import { faMoon,
+    faCodeBranch, 
+    faHome,
+    faBook,
+    faMusic,
+    faHistory,
+    faCode,
+    faUser
+} from "@fortawesome/free-solid-svg-icons";// Import the FontAwesomeIcon component
 import { useRouter } from 'next/router';
 
 import Banner from './banner';
@@ -27,26 +35,40 @@ export function Header (){
             <h2 className="font-bold text-2xl text-slate-800 w-[10%]">
                 <Link href='/'>Zwelar</Link>
             </h2>
-            <div className='flex justify-between w-[90%]'>
+            <div className='flex justify-between w-full'>
                 <nav className='w-[80%]'>
                     <ul className='flex flex-row gap-[1rem]'>
                         <li className=" text-md header-li">
                             <Link href='/'> 
-                            <span>
-                            <FontAwesomeIcon icon={faHome}>
-                            </FontAwesomeIcon>
+                            <span className='header-li-span'>
+                                <FontAwesomeIcon size='lg' icon={faHome}></FontAwesomeIcon>
                                 <span>Home</span>
+                            </span>
+                            </Link>
+                        </li>
+                        <li className="header-li">
+                            <Link href='/grammar/grammar'>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon size='lg' icon={faBook}></FontAwesomeIcon>
+                                    <span>Gramática</span>
                                 </span>
                             </Link>
                         </li>
                         <li className="header-li">
-                            <Link href='/grammar/grammar'>{`Gramática`}</Link>
+                            <Link href='/music'>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon size='lg' icon={faMusic}></FontAwesomeIcon>
+                                    <span>Música</span>
+                                </span>
+                            </Link>
                         </li>
                         <li className="header-li">
-                            <Link href='/music'>{`Música`}</Link>
-                        </li>
-                        <li className="header-li">
-                            <Link href='/history'>{`Histórias`}</Link>
+                            <Link href='/history'>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon size='lg' icon={faHistory}></FontAwesomeIcon>
+                                    <span>História</span>
+                                </span>
+                            </Link>
                         </li>
                         <li className="header-li">
                             <Link href='/courses'>{`Cursos`}</Link>
@@ -55,20 +77,28 @@ export function Header (){
                             <Link href='/about/about'>Sobre</Link>
                         </li>
                         <li  className="header-li">
-                            <Link href='/team'>Criadores</Link>
+                            <Link href='/team'>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon size='lg' icon={faCode}></FontAwesomeIcon>
+                                    <span>Criadores</span>
+                                </span>
+                            </Link>
                         </li>
                         <li  className="header-li">
-                            <Link href='#'>Perfil</Link>
+                            <Link href='#'>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon size='lg' icon={faUser}></FontAwesomeIcon>
+                                    <span>Perfil</span>
+                                </span>
+                            </Link>
                         </li>
                         <li  className="flex header-li">
-                            <span className='mr-2'>
-                                <FontAwesomeIcon icon={faCodeBranch}></FontAwesomeIcon>
-                            </span>
-                            <span className=''>
                                 <Link href='https://github.com/josymarss/kimbundu-technical-expressions'>
-                                    <a target="_blank">Github</a>
+                                <span className='header-li-span'>
+                                    <FontAwesomeIcon icon={faCodeBranch}></FontAwesomeIcon>
+                                     <span><a target="_blank">Github</a></span>
+                                </span> 
                                 </Link>
-                            </span> 
                         </li>
                         <li className="header-li">
                             <span>

3bc551567b7d3260017fecda8909798e3c6f8e39
rniochy <lniochy@gmail.com>
2023-05-24 02:13:26 +0100

    adding features into music page - editing music letter

--- app/music/page.jsx
+++ app/music/page.jsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 
 const Music = () => {
   const [lyric, setLyric] = useState()
+  const [pageNumber, setPageNumber] = useState(1);
   
 
   useEffect(()=>{
@@ -16,10 +17,17 @@ const Music = () => {
     console.log(singers)
   },[]);
 
+  const previousPageHandler = () => {
+      setPageNumber(pageNumber-1);
+  }
+  const nextPageHandler = () => {
+      setPageNumber(pageNumber+1);
+  }
+
   const MusicCard = ({author, title, imagUrl}) => {
     return(
-         <div className="w-[fit-content]  shadow-md bg-slate-50 p-2 mx-4 mt-4">
-         <div className='border-solid bg-cover rounded-sm transition-all opacity-80 bg-slate-900' >
+         <div className="w-[fit-content] shadow-md bg-slate-50 p-2 mx-4 mt-4">
+         <div className='border-solid bg-cover rounded-sm transition-all opacity-100 bg-slate-900' >
          <Image
                   width={150}
                   height={150}
@@ -39,36 +47,36 @@ const Music = () => {
 
   const MusicLetter = ({lyric, author, title}) => {
     return (      
-        <div className="lg:w-full">
+        <div className="w-[80%] flex flex-col flex-wrap">
             <h2 className="font-semibold text-[14pt] md:text-[16pt] text-slate-900 text-left">
               {title}
             </h2>
-            <div className='flex flex-col flex-wrap'>
-              <p className="text-[12pt] md:text-[14pt] text-green-500 mt-2 font-semibold">{author}</p>
+            <div className='flex flex-col flex-wrap text-center '>
+              <p className="text-[10pt] md:text-[12pt]  text-green-500 mt-2 font-semibold">{author}</p>
 
-              <p className="text-[12pt] md:text-[14pt]  text-justify mt-4">
+              <p className="text-[10pt] md:text-[12pt]  text-justify mt-4">
                   {`Alukenn n'golafua N'ga mu binga kia Muene ondo kala beniaba Eme n'gondodiame`}
               </p>
-              <p className="text-justify text-[12pt] md:text-[14pt]  w-96 mt-4">
+              <p className="text-justify text-[12pt] md:text-[14pt]  w-[100%] mt-4">
                 {`Mona mona muene Kissueia weza Mona mona muene Kalunga n'gumba`}
               </p>
-              <p className="text-justify text-[12pt] md:text-[14pt]  w-96 mt-4">
+              <p className="text-justify text-[12pt] md:text-[14pt]  w-[100%] mt-4">
                 {`N'zambi awani banack mona N'ga muvalele Muene ondo kala beniaba Eme
                 n'gondodiame`}
               </p>
             </div>
-            <button className="text-xl  text-500 mt-2 font-semibold">Traduzir</button>
+            {/* <button className="text-xl  text-500 mt-2 font-semibold">Traduzir</button> */}
       </div>
   );
   }
   return (
-    <div className="flex flex-col md:flex-row p-6 font-mulish w-full">
+    <div className="flex flex-col md:flex-row p-2 md:p-6 font-mulish w-full">
         {/* {/* MUISC LETTERS} */}
       <div className=''>
         <MusicLetter />
       </div>
 
-      <div className="">
+      <div className="sm:mt-5">
         <h2 className="text-slate-900 font-semibold  text-[12pt] mx-4 w-[100%]"> 
           Mais músicas 
         </h2>
@@ -82,11 +90,19 @@ const Music = () => {
             <MusicCard author="Bonga"  title="Mona Ki Ngi Xica" imagUrl='bonga.jpg'/>
             <MusicCard author="Edy Tussa"  title="Pekenina" imagUrl='edy.jpg'/> 
           </div>
-          {/* <div className='flex w-[100%]'>
-            <span>Anterior ...</span>
-            <span> 1 2 3 4 5 ...</span>
-            <span>Seguinte</span>
-          </div> */}
+          <div className='flex w-full'>
+            <span 
+              className='pagechange-button'
+              onClick={previousPageHandler}>
+                  Anterior
+            </span>
+            <span>{pageNumber}</span>
+            <span 
+              className='pagechange-button'
+              onClick={nextPageHandler}>
+                Seguinte
+            </span>
+          </div>
         </div>
       </div> 
      
